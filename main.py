64fc9c3d92531fe8bf4439e426cd1952158c6264
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-10 10:49:29 -0300

    feat: Adicionado a função cadastro

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+lista = []
+
+
+def cadastro():
+    nome = input("Digite o nome da rodovia: ")
+    extensao = float(input("Digite a Extensão dela: "))
+    ano = int(input("Digite o ano de criação dela: "))
+
+    lista.append({
+        "nome": nome,
+        "extensao": extensao,
+        "ano":  ano
+    })
+
+for i in range(2):
+    cadastro()
+
+print(lista)
