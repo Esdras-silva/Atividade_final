ed6d27ed997f80014fae19cb4c244fb8fe5d0794
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-12 10:13:57 -0300

    fix: Corigido um erro na função busca

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from utils.limitar_lista import limitar_lista
 
 
-lista = []
+lista = [{
+    "nome": "Arthur Bernades",
+    "extensao": 45.7,
+    "ano": 2024
+}, 
+{
+    "nome": "Julio cesar",
+    "extensao": 32.4,
+    "ano": 2000
+},{
+    "nome": "Perimetral",
+    "extensao": 24,
+    "ano": 2000
+}]
 
 
 
@@ -23,7 +36,7 @@ def cadastro():
 def busca():
     termo = input("digite a rodovia que deseja pesquisar: ").upper()#pega o termo digitado para busca e transforma em maiusculo
     for i in range(len(lista)):
-        if termo in lista[i]["nome"] or termo in str(lista[i]["ano"]) or termo in str(lista[i]["extensão"]):#Transforma os numeros em str para poder inteirar
+        if termo in lista[i]["nome"] or termo in str(lista[i]["ano"]) or termo in str(lista[i]["extensao"]):#Transforma os numeros em str para poder inteirar
             print(lista[i])
 
 
