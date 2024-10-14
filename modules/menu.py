8c6839bf543b7d64907e09cdd2db3bf28f46d8cc
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-14 02:01:25 -0300

    feat: criada a função menu, onde instancia toda as função criadas anteriormente

---
+++ modules/menu.py
@@ -0,0 +1,76 @@
+from utils.validacoes import confirmar_saida,validar_entrada
+from utils.timer import timer
+from cadastrar import cadastrar
+from impressao import imprimir_todas, imprimir_trecho
+from pesquisa import pesquisa
+def messagem_inicial() -> str :
+    return '''
+-------------------- BEM VINDO(A) AO REGISTRADOR DE RODOVIAS --------------------
+            Esse programa foi criado como proposta de trabalho final
+            de algoritmos da Profª Regiane Kawasaki. Por isso nos esforçamos
+            criar um ótimo programa, tentando seguir ao máximo as boas práticas
+            de programaçao.
+
+
+            By Antonio Silva e João Paulo Santos
+
+
+
+'''
+
+def opção_menu():
+    return '''
+-------------------- MENU --------------------
+
+                 1 - Cadastrar
+                 2 - Pesquisar
+                 3 - Imprimir
+                 4 - Sair
+
+'''
+
+def opção_imprimir():
+    return '''
+-------------------- IMPRIMIR --------------------
+                
+                1 - Impprimir Tudo
+                2 - Imprimir Trecho
+                3 - Sair
+
+'''
+
+def menu_imprimir():
+    opt_imprimir = 0
+    while opt_imprimir != 3:
+        print(opt_imprimir())
+        opt_imprimir = validar_entrada("Escolha uma opção: ")
+        if opt_imprimir == 1:
+            imprimir_todas()
+        elif opt_imprimir == 2:
+            imprimir_trecho()
+        elif opt_imprimir == 3:
+            print("Voltando...")
+            timer()
+    
+
+def menu_principal():
+    opt = None
+    while opt != 5:
+        try:
+            print(opção_menu())
+            opt = validar_entrada("Escolha uma opção: ")
+
+            if opt == 1:
+                cadastrar()
+            elif opt == 2:
+                pesquisa()
+            elif opt == 3:
+                menu_imprimir()
+            elif opt == 4:
+                if confirmar_saida():
+                    opt = 5
+                else:
+                    opt = None
+        except KeyboardInterrupt:
+            print("\n Vcê não popde interromper o programa pelo teclado. Para sair, escolha a opção 4.")
+            timer()
