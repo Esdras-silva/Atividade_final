5766c083dc9e54bb25346a2613747af98c0ab50d
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-14 01:16:35 -0300

    feat: Adicionada a função de pesquisa

---
+++ modules/pesquisa.py
@@ -0,0 +1,26 @@
+from database import buscar_rodovias
+def messagem_pesquisa() -> str:
+    return '''
+ ---------------------- PESQUISA ----------------------
+   Aqui você pode pesquisar as rodovias pelos seguintes
+   campos: 
+     Nome;
+     Extensao;
+     Ano de Criação.
+
+'''
+
+def pesquisa():
+   
+   try: 
+     print(messagem_pesquisa())
+     campo = input("Digite o em que campo você quer pesquisar: ").lower()
+     if campo in ["nome", "extensão","ano de criação"] :
+       termo = input(f"Digite o {campo} da Rodovia: ").lower()
+       
+       buscar_rodovias(campo,termo)
+     else:
+        raise Exception("Campo errado!!")
+     
+   except Exception as e:
+      print(e) 
