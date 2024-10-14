c2714ce902940fb2760b1c7537b8edda3b1d8f3d
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-14 01:42:28 -0300

    feat: criadas as funções de validações de entrada de menu

---
+++ utils/validacoes.py
@@ -0,0 +1,20 @@
+from timer import timer
+
+def validar_entrada(messagem) -> int :
+    while True:
+        try:
+            return int(input(messagem))
+        except ValueError:
+            print("Entrad invalida! Por favor, insira um número válido.")
+            timer()
+        
+
+def confirmar_saida():
+    while True:
+        sair = validar_entrada("Você tem certeza que quer sair do programa? (1 - Sim, 2 - Não): ")
+        
+        if sair == 1:
+            return True
+        elif sair == 2:
+            return False
+        
