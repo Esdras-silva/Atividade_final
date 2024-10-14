92c1f300eacf977e7c1b5b75c9760d8adac3f6ac
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-14 03:34:38 -0300

    feat: criada a função de cadastro de rodovia

---
+++ modules/cadastrar.py
@@ -0,0 +1,14 @@
+from database import adicionar_rodovia
+
+def cadastrar():
+     nome = input("Digite o nome da rodovia: ")
+     extensao = float(input("Digite a Extensão dela: "))
+     ano = int(input("Digite o ano de criação dela: "))
+
+     print(adicionar_rodovia({
+          "nome": nome,
+          "extensao": extensao,
+          "ano": ano
+     }))
+
+cadastrar()
