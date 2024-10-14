57b15363b8d4aaedb9dabf0d67923893789b9c94
Esdras Silva <esdras.antonio@outlook.com.br>
2024-10-14 03:18:04 -0300

    feat: Adicionada a função de imprimir elementos de uma lista

---
+++ utils/imprimir.py
@@ -0,0 +1,7 @@
+def imprimir(database: list, i: int):
+    exten = database[i]["extensão"]#coloca o item dentro da variavel
+    if type(exten) == float:#verifica se a variavel é do tipo float
+        print(f"{database[i]["nome"]}\t extensão: {database[i]["extensão"]:.3f} KM\t ano de criação: {database[i]["ano"]}")#se a variavel for float usa :.3f
+    else:
+            print(f"{database[i]["nome"]}\t extensão: {database[i]["extensão"]:>5} KM\t ano de criação: {database[i]["ano"]}")#se for int o numero é printado alinhado a direita
+            #esse if tem objetivos simplesmente esteticos para um print padronizado
